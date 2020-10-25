d33212f4e34c348be9ce7049abab0f13b06f1d5a
Minh Quan Do <minhquan98vn@gmail.com>
2020-10-25 11:09:00 +0700

    Put some quotes and make randomMessage function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello VS Code');
+const quotes = ['Waste no more time arguing what a good man should be. Be One. — Marcus Aurelius', 
+                'You could leave life right now. Let that determine what you do and say and think. — Marcus Aurelius',
+                'He who fears death will never do anything worth of a man who is alive. — Seneca',
+                'How long are you going to wait before you demand the best for yourself? — Epictetus',
+                'Wealth consists not in having great possessions, but in having few wants. ― Epictetus'];
+const randomMessage = arr => arr[Math.floor(Math.random() * 5)];
