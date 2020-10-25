e22501e715f2c797f0e58a4f472966a8ae513cc3
Minh Quan Do <minhquan98vn@gmail.com>
2020-10-25 11:19:41 +0700

    finish simple script program

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//five quotes to be displayed randomized
 const quotes = ['Waste no more time arguing what a good man should be. Be One. — Marcus Aurelius', 
                 'You could leave life right now. Let that determine what you do and say and think. — Marcus Aurelius',
                 'He who fears death will never do anything worth of a man who is alive. — Seneca',
                 'How long are you going to wait before you demand the best for yourself? — Epictetus',
                 'Wealth consists not in having great possessions, but in having few wants. ― Epictetus'];
+//make fuction to serve random message
 const randomMessage = arr => arr[Math.floor(Math.random() * 5)];
+//combine array and function to make complete program
+const displayMessage = (func,arr) => console.log(func(arr));
+//run with five quotes
+displayMessage(randomMessage,quotes);
