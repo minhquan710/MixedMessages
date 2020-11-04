7c841ce9184ac168155979f03f233326160b5ad3
Minh Quan Do <minhquan98vn@gmail.com>
2020-11-04 21:07:29 +0700

    Fix typo error

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const quotes = ['Waste no more time arguing what a good man should be. Be One. �
                 'He who fears death will never do anything worth of a man who is alive. — Seneca',
                 'How long are you going to wait before you demand the best for yourself? — Epictetus',
                 'Wealth consists not in having great possessions, but in having few wants. ― Epictetus'];
-//make simple fuction to serve random message
+// make simple function to serve random message
 const randomMessage = arr => arr[Math.floor(Math.random() * 5)];
 
-//combine array and function to make complete program
+// combine array and function to make complete program
 const displayMessage = (func,arr) => {
     let message = func(arr);
     let extend = "=".repeat(message.length + 8);
